579eb7d5532e67d285de215f7f90d3a78e0b91fc
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-12-30 18:09:28 -0500

    feat(standalone): simple server returning hello world html page

--- src/demo-server.js
+++ src/demo-server.js
@@ -1,8 +1,20 @@
 var express = require('express')
 var app = express()
 
+var simplePage = [
+  '<!DOCTYPE html>',
+  '<html lang="en">',
+  '<head>',
+  '<meta charset="utf-8">',
+  '</head>',
+  '<body>',
+  '<h1>Hello World</h1>',
+  '</body>',
+  '</html>'
+].join('\n')
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send(simplePage)
 })
 
 module.exports = app
