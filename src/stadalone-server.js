34de9dbea2579287c5ed8cfa04c534656635b28c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-12-30 18:04:29 -0500

    feat(server): stand alone server

---
+++ src/stadalone-server.js
@@ -0,0 +1,8 @@
+var app = require('./demo-server')
+
+var server = app.listen(3000, function () {
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+})
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  output: {
+    path: './dist',
+    filename: 'demo-server.js'
+  },
+  entry: {
+    'demo-server': './src/demo-server'
+  }
+}
