17efbad376a6155fc6dc96866428badfde0d5355
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-12-30 21:42:46 -0500

    chore(standalone): playing with response

--- src/stand-alone.js
+++ src/stand-alone.js
@@ -3,6 +3,19 @@ var http = require('http')
 
 function fn (req, res) {
   console.log('callback req', req.url)
+
+  const end = res.end
+
+  function endWithFinish (chunk, encoding) {
+    console.log('ending response for request', req.url)
+    console.log('output "%s ..."', chunk.toString().substr(0, 10))
+    console.log('%d %s %d', res.statusCode || 200,
+      res.get('Content-Type'),
+      res.get('Content-Length'))
+    end.apply(res, arguments)
+  }
+
+  res.end = endWithFinish
   app(req, res)
 }
 
